def90575804f723c5c52d8fc6204ef1c3faa13dc
Aayush <manandharaayush61@gmail.com>
2023-09-16 17:34:17 +0545

    popup component

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import AsBreakfast from "./Pages/Customer/ResturentPage/menuNavbar/asBreakfast";
 import Restcomponent from "./Pages/Customer/ResturentPage/Homepage/Restcomponent";
 import Resturent2ndComp from "./Pages/Customer/ResturentPage/Resturent2ndComp";
 import Topcomponent from "./Pages/Customer/ResturentPage/Homepage/Topcomponent";
+import Itempopup from "./Pages/Customer/ResturentPage/Itempopup";
 
 function App() {
   return (
     <>
       <Topcomponent />
       <Restcomponent />
+      <Resturent1stComp />
+      {/* <Itempopup /> */}
     </>
   );
 }
--- src/Pages/Customer/ResturentPage/Resturent2ndComp.js
+++ src/Pages/Customer/ResturentPage/Resturent2ndComp.js
@@ -3,13 +3,34 @@ import AsBreakfast from "./menuNavbar/asBreakfast";
 import AsSnacks from "./menuNavbar/asSnacks";
 import AsDrinks from "./menuNavbar/asDrinks";
 import AsFestival from "./menuNavbar/asFestival";
-import food from '../../Images/RestroPageImage/food2.jpg';
-import line from '../../Images/RestroPageImage/line.png';
-import bg from '../../Images/bg3.png';
+import food from "../../Images/RestroPageImage/food2.jpg";
+import line from "../../Images/RestroPageImage/line.png";
+import bg from "../../Images/bg3.png";
+import Itempopup from "./Itempopup";
 
 function Resturent2ndComp() {
+  //popup logic
+  const [popup, setPopup] = useState(false);
+  function openPopup() {
+    setPopup(true);
+  }
+  function closePopup() {
+    setPopup(false);
+  }
   const [role, setrole] = useState(1);
-  const [data, setData] = useState(["","","","","","","","","","",""]);
+  const [data, setData] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
 
   const itemsPerPage = 6; // Number of items to display per page
   const [currentPage, setCurrentPage] = useState(0);
@@ -31,79 +52,77 @@ function Resturent2ndComp() {
     currentPage * itemsPerPage + itemsPerPage
   );
 
-
   let componentToRender;
 
   switch (role) {
     case 1:
-      componentToRender = <AsBreakfast setrole={setrole}/>;
+      componentToRender = <AsBreakfast setrole={setrole} />;
       break;
     case 2:
-      componentToRender = <AsSnacks setrole={setrole}/>;
+      componentToRender = <AsSnacks setrole={setrole} />;
       break;
     case 3:
-      componentToRender = <AsDrinks setrole={setrole}/>;
+      componentToRender = <AsDrinks setrole={setrole} />;
       break;
     case 4:
-      componentToRender = <AsFestival setrole={setrole}/>;
+      componentToRender = <AsFestival setrole={setrole} />;
       break;
     default:
-      componentToRender = <AsBreakfast setrole={setrole}/>
+      componentToRender = <AsBreakfast setrole={setrole} />;
       break;
   }
-  return <> {componentToRender}
- <div className=" backgroundImg mt-5">
-  <div className=" flex justify-end mx-[145px] mb-4 gap-3">
-  <button className="bg-[#EC2633] px-2 text-white" onClick={handlePrev}>
-          <i className="fa-solid fa-angle-left"></i>
-        </button>
-        <button className="bg-[#FF9800] px-2 text-white" onClick={handleNext}>
-          <i className="fa-solid fa-angle-right"></i>
-        </button>
-  </div>
-
-  <div className="flex flex-wrap gap-5 items-center mx-[120px] justify-around">
-    
-
-    {
-      displayedItems.map((value,index)=>(
-
-        <div key={index} className="border-2 h-[400px] backgroundImg1 border-black w-[330px] overflow-hidden">  
-      <div className="relative h-[280px] overflow-hidden ">
-        <img
-          className="h-[280px] w-[650px] transition-transform transform scale-100 hover:scale-110"
-          src={food}
-          alt=""
-        />
-      </div>
-      <div className="dhamilo flex flex-col justify-center items-center text-white  h-[120px]">
-        
-        <div className="text-[#d4f532] flex gap-1">
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
-        <i className="fa-solid fa-star"></i>
+  return (
+    <>
+      {" "}
+      {componentToRender}
+      <div className=" backgroundImg mt-5">
+        <div className=" flex justify-end mx-[145px] mb-4 gap-3">
+          <button className="bg-[#EC2633] px-2 text-white" onClick={handlePrev}>
+            <i className="fa-solid fa-angle-left"></i>
+          </button>
+          <button className="bg-[#FF9800] px-2 text-white" onClick={handleNext}>
+            <i className="fa-solid fa-angle-right"></i>
+          </button>
         </div>
-        <h1>Samaya Bhajiya</h1>
-        <div className="mx-4"><img src={line} alt="" /></div>
-        <p>Rs 350</p>
+
+        <div className="flex flex-wrap gap-5 items-center mx-[120px] justify-around">
+          {displayedItems.map((value, index) => (
+            <div
+              key={index}
+              className="border-2 h-[372px] backgroundImg1 border-red-600 w-[360px] overflow-hidden"
+            >
+              <div className="relative h-[250px] overflow-hidden ">
+                <img
+                  className="h-[250px] w-[650px] transition-transform transform scale-100 hover:scale-105"
+                  src={food}
+                  alt=""
+                  onClick={openPopup}
+                />
+                {popup && <Itempopup onClose={closePopup} />}
+              </div>
+              <div className="dhamilo flex flex-col justify-center items-center text-white  h-[120px]">
+                <div className="text-[#FEBB41] flex gap-1">
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </div>
+                <h1>Samay Baji</h1>
+                <div className="mx-4">
+                  <img src={line} alt="" />
+                </div>
+                <p>Rs 350</p>
+              </div>
+            </div>
+          ))}
+
+          {/* this is a comp */}
+          <img className="absolut h-[600px] w-[1700px] " src={bg} alt="" />
+        </div>
       </div>
-    </div>
-
-      ))
-    }
-
-    {/* this is a comp */}
-    <img className="absolut h-[600px] w-[1700px] " src={bg} alt="" />
-
-    
-
-  </div>
-  
-</div>
-
-  </>;
+    </>
+  );
 }
 
 export default Resturent2ndComp;
