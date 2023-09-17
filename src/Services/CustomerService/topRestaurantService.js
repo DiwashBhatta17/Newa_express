f8bf8430ca191689126e9e27ea9f6c12919024e1
DiwashBhatta17 <bhattaaawash@gmail.com>
2023-09-17 22:04:42 +0545

    top restro service complete

---
+++ src/Services/CustomerService/topRestaurantService.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import baseURL from "../Api/api";
+async function topRestaurantService() {
+    try {
+        const response = await axios.get(baseURL+"restaurants/topRestaurant");
+        return response.data;
+        
+    } catch (error) {
+        throw error;
+        
+    }  
+}
+
+ async function getBannerImage(restaurantId) {
+    try {
+      const response = await axios.get(
+        baseURL + "restaurants/getRestaurantBannerImage/" + restaurantId,
+        {
+          responseType: "arraybuffer", // Specify the response type as arraybuffer
+        }
+      );
+  
+      const imageBlob = new Blob([response.data], {
+        type: response.headers["content-type"],
+      });
+  
+      return URL.createObjectURL(imageBlob);
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+   async function getProfileImage(restaurantId) {
+    try {
+      const response = await axios.get(
+        baseURL + "restaurants/getRestaurantPic/" + restaurantId,
+        {
+          responseType: "arraybuffer", 
+        }
+      );
+  
+      const imageBlob = new Blob([response.data], {
+        type: response.headers["content-type"],
+      });
+  
+      return URL.createObjectURL(imageBlob);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  export async function getTopRestaurant(){
+   try {
+    const response = await topRestaurantService();
+    if(response){
+      
+      for(let i = 0; i< response.length; i++){
+        let bannerImg = await getBannerImage(response[i]?.restaurantId);
+        let profileImg = await getProfileImage(response[i]?.restaurantId);
+        response[i]["bannerImg"] = bannerImg;
+        response[i]["profileImg"] = profileImg;  
+      }
+      return response;
+    }
+    
+   } catch (error) {
+    throw error
+    
+   }
+
+  }
+export default getTopRestaurant;
