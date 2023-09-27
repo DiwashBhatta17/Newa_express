b5778e794ee710f5f0e8712b00de5ee32b3214be
DiwashBhatta17 <bhattaaawash@gmail.com>
2023-09-27 08:11:15 +0545

    some changes

--- src/Pages/Customer/BrowseRestropage/Browse2.js
+++ src/Pages/Customer/BrowseRestropage/Browse2.js
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import restu from "../../Images/RestroPageImage/bgRestu.png";
-import CustomerNavbar from '../customerNavbar';
-import Footer from '../Footer';
-import getAllrestaurantService, {getBannerImage, getProfileImage} from '../../../Services/CustomerService/getAllrestaurantService';
-import searchRestaurantService from '../../../Services/CustomerService/searchRestaurantService';
-
+import CustomerNavbar from "../customerNavbar";
+import Footer from "../Footer";
+import getAllrestaurantService, {
+  getBannerImage,
+  getProfileImage,
+} from "../../../Services/CustomerService/getAllrestaurantService";
+import searchRestaurantService from "../../../Services/CustomerService/searchRestaurantService";
 
 function Browse2() {
-  const [data, setData] = useState(["","","","","","",])
-  const query = useParams()
-
+  const [data, setData] = useState(["", "", "", "", "", ""]);
+  const { query } = useParams();
 
   async function fetchRestaurant() {
-
     try {
-  
       const response = await getAllrestaurantService();
 
-    
-     
       if (response) {
-        console.log(response);
         for (let i = 0; i < response.length; i++) {
           let bannerImg = await fetchBannerImage(response[i]?.restaurantId);
           let profileImg = await fetchProfileImage(response[i]?.restaurantId);
           response[i]["bannerImg"] = bannerImg;
-          response[i]["profileImg"]= profileImg;
+          response[i]["profileImg"] = profileImg;
         }
-        setData(response);
+
+        const results = response?.filter((restaurant) => {
+          return restaurant?.restaurantName?.toLowerCase().includes(query);
+        });
+
+        results && setData(results);
       }
     } catch (error) {
       console.error("Cannot get the News", error);
@@ -55,65 +56,64 @@ function Browse2() {
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchRestaurant();
-  },[]);
-
-    
+    // performSearch();
+  }, []);
 
   return (
-    <div className='backgroundImg1 mb-5'> 
-    <div className='py-5 px-[120px]'>
-        <img className='w-[30%]' src="/Image/restroList.png" alt="" />
-    </div>
-       <div className='pb-[280px]'>
-       
+    <div className="backgroundImg1 mb-5">
+      <div className="py-5 px-[120px]">
+        <img className="w-[30%]" src="/Image/restroList.png" alt="" />
+      </div>
+      <div className="pb-[280px]">
         <div className="flex backgroundImg2 py-5 flex-wrap gap-3 items-cente justify-center mx-[110px] ">
           {data.map((value, index) => (
             <Link to={`/restaurant/${value.restaurantId}`}>
-            <div
-              key={index}
-              className="border-2  h-[300px] backgroundImg1 border-black w-[280px] overflow-hidden"
-            >
-              <div className="h-[190px] overflow-hidden ">
-                <img
-                  className="h-[240px] -z-30 w-[650px] transition-transform transform  scale-110 hover:scale-125"
-                  src={value.bannerImg}
-                  alt=""
-                />
-              </div>
-              <div className=" flex item-center justify-center absolute ml-[90px] -mt-[80px]">
-                
-                <img className=" h-[100px] w-[100px] rounded-full" src={value.profileImg} alt="" />
-              </div>
-
-              <div className="bg-[#0e0e0e97] flex flex-col justify-center items-center text-white  h-[130px]">
-                <div className="text-[#d4f532] flex gap-1">
-                  {/* <i className="fa-solid fa-star"></i> */}
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
+              <div
+                key={index}
+                className="border-2  h-[300px] backgroundImg1 border-black w-[280px] overflow-hidden"
+              >
+                <div className="h-[190px] overflow-hidden ">
+                  <img
+                    className="h-[240px] -z-30 w-[650px] transition-transform transform  scale-110 hover:scale-125"
+                    src={value.bannerImg}
+                    alt=""
+                  />
+                </div>
+                <div className=" flex item-center justify-center absolute ml-[90px] -mt-[80px]">
+                  <img
+                    className=" h-[100px] w-[100px] rounded-full"
+                    src={value.profileImg}
+                    alt=""
+                  />
                 </div>
 
+                <div className="bg-[#0e0e0e97] flex flex-col justify-center items-center text-white  h-[130px]">
+                  <div className="text-[#d4f532] flex gap-1">
+                    {/* <i className="fa-solid fa-star"></i> */}
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                  </div>
 
-                <h1 className="text-[#f24d4d] text-xl font-bold">{value.restaurantName}</h1>
-                <p className="text-[#e2dddd]">{value.restaurantAddress}</p>
-                <div className="mx-4">{/* <img src={line} alt="" /> */}</div>
-                
+                  <h1 className="text-[#f24d4d] text-xl font-bold">
+                    {value.restaurantName}
+                  </h1>
+                  <p className="text-[#e2dddd]">{value.restaurantAddress}</p>
+                  <div className="mx-4">{/* <img src={line} alt="" /> */}</div>
+                </div>
               </div>
-            </div>
             </Link>
           ))}
 
           {/* this is a comp */}
         </div>
-      
-       </div>
-       <Footer/>
-      
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Browse2;
